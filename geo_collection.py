f469f1921192434bcab9cf3e9b9b890007e0c167
An-geles <66815800+An-geles@users.noreply.github.com>
2023-02-03 14:23:08 -0800

    add: geo collection using lat, lng from geo_generator

---
+++ geo_collection.py
@@ -0,0 +1,50 @@
+
+# required imports for this python script
+
+# secrets.py hosts API keys required for google maps API requests
+from key import API_key
+from geo_generator import coordList
+# # not needed and can be unsigned but if user prefers secrets.py also hosts signature key,
+# # encountered errors using URL signature
+# from secrets import URL_sign
+
+# # required imports for this python script
+import requests
+import time
+import os
+
+folder_name = "images"
+current_dir = os.path.dirname(os.path.abspath(__file__))
+folder_path = os.path.join(current_dir, folder_name)
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+    print(f"successfully saved images in sub-folder {folder_name} in {current_dir}.")
+else:
+    print(f"Sub-folder {folder_name} already exists in {current_dir}")
+
+
+# location variables in terms of longitude and latitude needed for the Google Maps API parameters
+
+for i in range(len(coordList)):
+    value = coordList[i]
+    lat, lng = value['lat'], value['lng']
+    pitch = 0
+
+# # step stops 45 before so 360 + 45, iterative loop to update the heading parameter
+# # to pull a full set of static images totaling 360 degrees for image stitching
+    for h in range(0, 405, 45):
+        heading = h
+        url = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location={1},{2}&heading={3}&pitch=-0.76&key={0}".format(
+            API_key, lat, lng, heading)
+
+        # section of code that will save the pulled images in a previously made with the filename format of
+        # latitude-longitude-degree.jpg
+        filename = "images/{0}-{1}-{2}.jpg".format(lat, lng, heading)
+        response = requests.get(url)
+        with open(filename, "wb") as f:
+            f.write(response.content)
+        # add time buffer for API
+        time.sleep(1)
+
+
