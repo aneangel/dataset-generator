7254af90d0a4fb2bf006f276eacd6a34bef7aac7
An-geles <66815800+An-geles@users.noreply.github.com>
2023-02-03 14:00:38 -0800

    add: city data

---
+++ geo_generator.py
@@ -0,0 +1,35 @@
+from opencage.geocoder import OpenCageGeocode
+# import time
+
+key = 'bcd6b1bdf8694408b584ba1132a42fad'
+
+geoCoder = OpenCageGeocode(key)
+
+cities_dict = {'cities': ['San Jose', 'San Francisco', 'Sunnyvale', 'Campbell','Berkeley', 'Oakland',
+                          'Saratoga', 'Cupertino', 'Palo Alto', 'Los Altos', 'Milpitas', 'Fremont', 'Santa Clara',
+                          'San Mateo', 'Alameda', 'Walnut Creek', 'Fremont', 'Sacramento', 'Los Angeles', 'Santa Cruz',
+                          'Gilroy', 'Daly city', 'Concord', 'Stockton', 'Dublin', 'Danville', 'Hayward', 'Glroy', 'Lafayette',
+                          'Livermore', 'Menlo Park', 'Los Gatos', 'Moraga', 'Mountain View', 'Morgan Hill', 'Pleasant Hill',
+                          'Richmond', 'Redwood City', 'San Bruno', 'Pleasanton', 'Piedmont', 'Sonoma', 'Union City', 'Los Altos Hills',
+                          'Dixon', 'Dublin', 'Half Moon Bay', 'Burlingame', 'Antioch', 'Atherton']}
+
+list_lat = []
+list_long = []
+
+counter = 0
+
+# results = geocoder.geocode(query)
+# print(results)
+
+coordList = []
+
+for city in cities_dict['cities']:
+    counter += 1
+    query = city + ',' + 'California'
+    results = geoCoder.geocode(query)
+    coordList.append(results[0]['geometry'])
+    print('{0} found location data'.format(city))
+
+
+print(coordList)
+print(coordList[0])
